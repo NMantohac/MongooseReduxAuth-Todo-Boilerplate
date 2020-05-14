7a81a8166f19ce90e809ea7dd75205f240bd5b92
Norberto Mantohac <nmantohac82098@gmail.com>
2020-05-13 17:39:46 -0700

    initialized repo: back-end set up with password encryption

---
+++ controllers/userController.js
@@ -0,0 +1,30 @@
+const { Todo, User } = require('./../models');
+
+module.exports = {
+  addTodo: async (req, res) => {
+    const { text } = req.body;
+    if (!text) {
+      return res.status(400).json({ error: 'You must provide a test' });
+    }
+
+    try {
+      const newTodo = await new Todo({ text, user: req.user._id }).save();
+      req.user.todos.push(newTodo);
+      await req.user.save();
+      return res.status(200).json(newTodo);
+    } catch (e) {
+      return res.status(403).json(e);
+    }
+  },
+  getAllUserEmails: async (req, res) => {
+    try {
+      const users = await User.find({}, 'email');
+      if (!users) {
+        return res.status(404).json({ error: 'No user found' })
+      }
+      return res.status(200).json(users);
+    } catch (e) {
+      return res.status(403).json(e);
+    }
+  }
+}
---
+++ routes/apiRoutes/userRoutes/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const { addTodo, getAllUserEmails } = require('./../../../controllers/userController');
+
+const { requireAuth } = require('./../../../middlewares/authMiddleware');
+
+// /api/user
+
+// /api/user/todos
+router.route('/todos')
+  .post(requireAuth, addTodo);
+
+// /api/user/emails
+router.get('/emails', getAllUserEmails);
+
+module.exports = router;
